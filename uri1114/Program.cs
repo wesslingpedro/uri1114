1964e4cabda3f7c4671afa34734481995c1f3f23
wesslingpedro <wesslingpedro@gmail.com>
2024-05-28 19:59:21 -0300

    Primeiro commit

---
+++ uri1114/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace testes_ec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int senha;
+
+            senha = int.Parse(Console.ReadLine());
+
+            while (senha != 2002)
+            {
+                Console.WriteLine("Senha Invalida");
+                senha = int.Parse(Console.ReadLine());
+            }
+            if (senha == 2002)
+            {
+                Console.WriteLine("Acesso Permitido");
+            }
+
+
+
+
+        }
+
+    }
+
+}
